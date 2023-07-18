9cc286feb5c67eb9b5283ca1a1926df90a4adf02
pham-phuc <101715209+pham-phuc@users.noreply.github.com>
2023-07-18 20:35:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
 function App() {
+  const [seconds, setSeconds] = useState(10);
+  const [isActive, setIsActive] = useState(false);
+
+  function active() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(10);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>TIMING CLOCK</h1>
+      <div className="timing-clock">
+        <div className="time">{seconds <= 0 ? 'Hết Giờ' : seconds}</div>
+        <div className="control">
+          <button
+            className={`button button-primary button-primary-${
+              isActive ? "active" : "inactive"
+            }`}
+            onClick={active}
+          >
+            {isActive ? "Pause" : "Start"}
+          </button>
+          <button className="button" onClick={reset}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
